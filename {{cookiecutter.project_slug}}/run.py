d55276d5533f906ef869fb5bd94cbc86e3fac0c5
tintinrevient <zhaobenben007@googlemail.com>
2025-04-24 13:55:06 +0200

    add docker compose to mount volumes

--- {{cookiecutter.project_slug}}/run.py
+++ {{cookiecutter.project_slug}}/run.py
@@ -1,6 +1,42 @@
-from {{ cookiecutter.project_slug }}.method import predict
+import pandas as pd
+from random import random
+import json
+from loguru import logger
+import click
+
+@click.group()
+def cli():
+    pass
+
+@cli.command(name="predict", help="predict the targets from the input dataframes")
+@click.option("--input-file", help="the input CSV dataframe")
+@click.option("--output-file", help="the output JSON predicted targets")
+def predict(input_file: str, output_file: str):
+
+    logger.info(f"Reading from {input_file}")
+    logger.info(f"Generating outputs")
+
+    data = pd.read_csv(input_file)
+    target = "attr1"
+    
+    output = {
+        "actual": [float(v) for v in data["attr1"].values],
+        "predicted": list([random() for i in range(data.shape[0])]),
+        "model": {
+            "name": "random heuristic",
+            "params": {
+                "seed": 0
+            }
+        },
+        "metadata": {
+            "dataset": input_file
+        }
+    }
+
+    with open(output_file, "w") as file:
+        json.dump(output, file)
+
+    logger.info(f"Done. Check {output_file}")
 
 if __name__ == "__main__":
-    print("Generating outputs")
-    predict()
-    print("Done. Check data/outputs.json")
+    cli()
