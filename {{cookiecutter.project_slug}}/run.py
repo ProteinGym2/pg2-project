1af8f541e1491e669b76ee98a3d471b36dab5f13
Caio Benatti Moretti <caiodba@gmail.com>
2025-04-29 14:33:57 +0200

    adapting project to manifest.json

--- {{cookiecutter.project_slug}}/run.py
+++ {{cookiecutter.project_slug}}/run.py
@@ -1,17 +1,20 @@
 #!/usr/local/bin/python
 
 import pandas as pd
-from random import random
+import random
 import json
 from loguru import logger
 import typer
 
 app = typer.Typer()
 
+with open("manifest.json") as f:
+    config = json.load(f)
+
 @app.command()
 def predict(
     input_file: str = "data/sample.csv",
-    output_file: str = "data/output.json"
+    output_file: str = "data/output.csv"
 ):
 
     logger.info(f"Reading from {input_file}")
@@ -20,22 +23,16 @@ def predict(
     data = pd.read_csv(input_file)
     target = "attr1"
     
+    random.seed(config["model"]["params"]["seed"])
     output = {
+        "sequence": [v for v in data["sequence"].values],
         "actual": [float(v) for v in data["attr1"].values],
-        "predicted": list([random() for i in range(data.shape[0])]),
-        "model": {
-            "name": "random heuristic",
-            "params": {
-                "seed": 0
-            }
-        },
-        "metadata": {
-            "dataset": input_file
-        }
+        "predicted": list([random.random() for i in range(data.shape[0])]),
+        "split": [v for v in data["split"].values],
     }
 
-    with open(output_file, "w") as file:
-        json.dump(output, file)
+    output_df = pd.DataFrame(output)
+    output_df.to_csv(output_file)
 
     logger.info(f"Done. Check {output_file}")
 
